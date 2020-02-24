674c6baacb5b31b26152a538f206e5077b3ad91a
Sarah-hui <2279789508@qq.com>
2020-02-24 20:27:14 +0800

    视图微服务—Ribbon

---
+++ productViewServiceRibbon/src/main/java/springcloud/web/ProductController.java
@@ -0,0 +1,24 @@
+package springcloud.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springcloud.pojo.Product;
+import springcloud.service.ProductService;
+
+@RestController
+public class ProductController {
+    @Autowired
+    ProductService productService;
+
+    @RequestMapping("/products")
+    public Object products(Model m) {
+        List<Product> ps = productService.listProducts();
+        m.addAttribute("ps", ps);
+        return "products";
+    }
+}
