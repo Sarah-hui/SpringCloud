9a50e89c67ca0da654c75bf0f3a61cb7c21b8fc6
Sarah-hui <2279789508@qq.com>
2020-02-24 21:23:58 +0800

    视图微服务—feign

---
+++ productViewServiceFeign/src/main/java/feign/client/ProductClientFeign.java
@@ -0,0 +1,13 @@
+package feign.client;
+
+import feign.pojo.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(value = "PRODUCT-DATA-SERVICE")
+public interface ProductClientFeign {
+    @GetMapping("/products")
+    public List<Product> listProdcuts();
+}
---
+++ productViewServiceFeign/src/main/java/feign/service/ProductService.java
@@ -0,0 +1,18 @@
+package feign.service;
+
+import feign.client.ProductClientFeign;
+import feign.pojo.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductClientFeign productClientFeign;
+    public List<Product> listProducts(){
+        return productClientFeign.listProdcuts();
+
+    }
+}
