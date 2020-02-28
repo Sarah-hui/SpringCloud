93696fdebfa30c636d77e6c8448766b5ec5bae84
Sarah-hui <2279789508@qq.com>
2020-02-28 20:16:27 +0800

    rabbitmq fanout 模式

---
+++ src/main/java/cn/how2j/RabbitMQUtil.java
@@ -0,0 +1,16 @@
+package cn.how2j;
+
+import javax.swing.*;
+import cn.hutool.core.util.NetUtil;
+
+public class RabbitMQUtil {
+    public static void main(String[] args) {
+        checkServer();
+    }
+    public static void checkServer() {
+        if(NetUtil.isUsableLocalPort(15672)) {
+            JOptionPane.showMessageDialog(null, "RabbitMQ 服务器未启动 ");
+            System.exit(1);
+        }
+    }
+}
