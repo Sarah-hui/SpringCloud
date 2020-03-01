fb717589a4f73ff81ecdac5964753a4e5b05c7c5
Sarah-hui <2279789508@qq.com>
2020-03-01 15:49:21 +0800

    断路器监控

--- productViewServiceFeign/src/main/java/feign/ProductViewServiceFeignApplication.java
+++ productViewServiceFeign/src/main/java/feign/ProductViewServiceFeignApplication.java
@@ -9,6 +9,7 @@ import java.util.concurrent.TimeoutException;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -23,6 +24,7 @@ import java.util.concurrent.TimeUnit;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class ProductViewServiceFeignApplication {
     public static void main(String[] args) {
         //判断 rabiitMQ 是否启动
--- productViewServiceFeign/src/main/java/feign/client/ProductClientFeign.java
+++ productViewServiceFeign/src/main/java/feign/client/ProductClientFeign.java
@@ -1,12 +1,13 @@
 package feign.client;
 
+import feign.hystrix.ProductClientFeignHystrix;
 import feign.pojo.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(value = "PRODUCT-DATA-SERVICE")
+@FeignClient(value = "PRODUCT-DATA-SERVICE",fallback = ProductClientFeignHystrix.class)
 public interface ProductClientFeign {
     @GetMapping("/products")
     public List<Product> listProdcuts();
